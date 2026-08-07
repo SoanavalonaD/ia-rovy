e0b401be7e9f4216274cc4f2ede51290e77439dd
SoanavalonaD <deborah.razafindrabezandrina@esti.mg>
2026-08-07 22:47:03 +0300

    fix: adding logs to see the comment

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ app.get('/webhook', (req, res) => {
 // 2. Réception des événements (POST)
 app.post('/webhook', async (req, res) => {
   const body = req.body;
+  console.log("Webhook POST reçu :", JSON.stringify(body, null, 2));
 
   if (body.object === 'page') {
     body.entry.forEach(async (entry) => {
